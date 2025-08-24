a7680e472725dcaa3a205ce700e31f2d1fa51478
Anton Bocan <bocan.anton@mail.ru>
2025-08-24 17:55:26 +0500

    Remove Babel config and simplify Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': '.',
-    };
-    return config;
-  },
+
   images: {
     remotePatterns: [
       {
